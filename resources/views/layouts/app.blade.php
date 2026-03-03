532c33d1591c2373094ebaf1771f3af01e27a349
Sakuja Shamal Gajanayake <2021sp026@univ.jfn.ac.lk>
2026-03-03 14:29:39 +0530

    test1

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title')</title>
+</head>
+<body>
+
+    <nav>
+        <h2>My Stock Management System</h2>
+        <hr>
+    </nav>
+
+    <div>
+        @yield('content')
+    </div>
+
+    <footer>
+        <hr>
+        <p>© 2026 Sakuja</p>
+    </footer>
+
+</body>
+</html>
